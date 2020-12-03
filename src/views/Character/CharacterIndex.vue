308be4a9cf0a60b91100f989485d416ebc52613c
lbavendanos <lbavendanos@gmail.com>
2020-12-02 21:04:31 -0500

    Add character section

--- src/views/Character/CharacterIndex.vue
+++ src/views/Character/CharacterIndex.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="container mx-auto p-5">
-    <h1 class="text-center">This is an Characters page</h1>
+  <div class="container pt-16 mx-auto">
+    <div class="flex flex-col text-center w-full mb-5">
+      <h1
+        class="title sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+      >
+        CHARACTERS
+      </h1>
+      <p class="subtitle lg:w-2/3 mx-auto leading-relaxed text-base">
+        In this section you can search for a character
+      </p>
+    </div>
+  </div>
+  <div class="container mx-auto">
+    <FormSearch @submit="onSubmit" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import FormSearch, { FormSearchData } from '@/components/FormSearch.vue'
 
 export default defineComponent({
-  name: 'CharacterIndex'
+  name: 'CharacterIndex',
+  components: { FormSearch },
+  setup() {
+    const onSubmit = (formData: FormSearchData) => {
+      console.log(formData)
+    }
+
+    return { onSubmit }
+  }
 })
 </script>
--- tests/unit/views/character/character-index.spec.ts
+++ tests/unit/views/character/character-index.spec.ts
@@ -2,9 +2,19 @@ import { shallowMount } from '@vue/test-utils'
 import CharacterIndex from '@/views/Character/CharacterIndex.vue'
 
 describe('CharacterIndex.vue', () => {
-  it('render character index', () => {
+  it('render title', () => {
     const wrapper = shallowMount(CharacterIndex)
+    const title = wrapper.get('.title')
 
-    expect(wrapper.html()).toContain('Characters')
+    expect(title.text()).toBe('CHARACTERS')
+  })
+
+  it('render subtitle', () => {
+    const wrapper = shallowMount(CharacterIndex)
+    const subtitle = wrapper.get('.subtitle')
+
+    expect(subtitle.text()).toBe(
+      'In this section you can search for a character'
+    )
   })
 })
