4bfb748e686523a8b404741a357677c182337905
lbavendanos <lbavendanos@gmail.com>
2020-12-18 19:16:21 -0500

    Refactor test

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,21 +1,17 @@
 import { mount } from '@vue/test-utils'
-import router from '@/router'
 import App from '@/App.vue'
 
 function wrapperFactory(options = {}) {
   return mount(App, {
     global: {
-      plugins: [router]
+      stubs: ['router-view', 'router-link']
     },
     ...options
   })
 }
 
 describe('App.vue', () => {
-  it('render main section', async () => {
-    router.push('/')
-    await router.isReady()
-
+  it('render main section', () => {
     const wrapper = wrapperFactory()
     const main = wrapper.find('main')
 
